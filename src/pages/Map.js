5f1260feb444913bd545c4d80dfada0e8923396c
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2024-11-05 18:38:49 +1100

    Add fetch logic

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,31 +1,100 @@
 import React, { useEffect, useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
 import AusMap from '../components/AusMap';
+import * as d3 from 'd3';
+import { Typography, FormControl, Select, MenuItem, Stack, TextField, InputLabel } from '@mui/material';
+
+const mapNameToConfig = {
+  'Rain_mm': {
+    displayName: 'Rainfall (mm)',
+    colorInterpolate: d3.interpolateBlues,
+    domain: null
+  },
+  'Temp_Max': {
+    displayName: 'Maximum Temperature (°C)',
+    colorInterpolate: d3.interpolateRgbBasis(['white', 'yellow', 'red']),
+    domain: [-10, 50],
+  },
+  'Temp_Min': {
+    displayName: 'Minimum Temperature (°C)',
+    colorInterpolate: d3.interpolateRgbBasis(['white', 'yellow', 'red']),
+    domain: [-10, 50],
+  },
+}
+
+const ausStates = [ 'NSW', 'VIC', 'QLD', 'SA', 'WA', 'TAS', 'NT', 'ACT' ];
 
 const Map = () => {
-  const [location, setLocation] = useOutletContext();
+  const [weatherType, setWeatherType] = useState('Rain_mm');
+  const [date, setDate] = useState('2023-08-01');
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // fetch('https://api.weather.gov.au/rainfall')
-    //   .then(response => response.json())
-    //   .then(data => setData(data));
-
-    // Mock data
     setData([
-      { 'State': 'NSW', 'Temp_Max': 20 },
-      { 'State': 'VIC', 'Temp_Max': 30 },
-      { 'State': 'QLD', 'Temp_Max': 40 },
-      { 'State': 'SA', 'Temp_Max': 50 },
-      { 'State': 'WA', 'Temp_Max': 60 },
-      { 'State': 'TAS', 'Temp_Max': 70 },
-      { 'State': 'NT', 'Temp_Max': 80 },
-      { 'State': 'ACT', 'Temp_Max': 90 },
+      { 'State': 'NSW', [weatherType]: 0 },
+      { 'State': 'VIC', [weatherType]: 0 },
+      { 'State': 'QLD', [weatherType]: 0 },
+      { 'State': 'SA', [weatherType]: 0 },
+      { 'State': 'WA', [weatherType]: 0 },
+      { 'State': 'TAS', [weatherType]: 0 },
+      { 'State': 'NT', [weatherType]: 0 },
+      { 'State': 'ACT', [weatherType]: 0 },
     ]);
-  }, []);
+
+    for (const state of ausStates) {
+      fetch(`http://localhost:8000/api/data/weather?state=${state}&weatherType=${weatherType}&fromDate=${date}&toDate=${date}`)
+      .then(res => res.json())
+      .then(res => {
+        setData(data => data.map(d => {
+          if (d['State'] === state) {
+            return { 'State': state, [weatherType]: res[0][weatherType] };
+          }
+          return d;
+        }));
+      });
+    }
+
+  }, [weatherType, date]);
 
   return (
-    <AusMap data={data} dataName='Temp_Max' displayName='Maximum Temperature (°C)' />
+    <>
+      <Typography variant='h4' gutterBottom>
+        Map of Australia
+      </Typography>
+
+      <Stack direction='row' spacing={2} sx={{ mb: 2 }}>
+        <TextField
+          label="Select Date"
+          type="date"
+          value={date}
+          onChange={e => setDate(e.target.value)}
+          slotProps={{
+            inputLabel: { shrink: true },
+            htmlInput: { min: '2023-08-01', max: '2024-09-23' }
+          }}
+          fullWidth
+        />
+
+        <FormControl variant='outlined' fullWidth>
+          <InputLabel>Weather Type</InputLabel>
+          <Select
+            label='Weather Type'
+            value={weatherType}
+            onChange={e => setWeatherType(e.target.value)}
+          >
+            <MenuItem value='Rain_mm'>Rainfall (mm)</MenuItem>
+            <MenuItem value='Temp_Max'>Maximum Temperature (°C)</MenuItem>
+            <MenuItem value='Temp_Min'>Minimum Temperature (°C)</MenuItem>
+          </Select>
+        </FormControl>
+      </Stack>
+
+      <AusMap
+      data={data}
+      dataName={weatherType}
+      displayName={mapNameToConfig[weatherType].displayName}
+      colorInterpolate={mapNameToConfig[weatherType].colorInterpolate}
+      domain={mapNameToConfig[weatherType].domain}/>
+    </>
   );
 };
 
